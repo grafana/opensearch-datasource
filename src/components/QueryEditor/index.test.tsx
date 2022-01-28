205c25bff01a7b78f1c792f22b255093f1fea936
Elfo404 <me@giordanoricci.com>
2022-01-28 15:55:47 +0000

    Fix alias pattern not being correctly handled by Query Editor

--- src/components/QueryEditor/index.test.tsx
+++ src/components/QueryEditor/index.test.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { QueryEditorForm } from './';
-import { LuceneEditor } from './LuceneEditor';
-import { PPLEditor } from './PPLEditor';
 import { OpenSearchQuery, QueryType } from '../../types';
 
+import { render, screen } from '@testing-library/react';
+import { QueryEditor } from '.';
+import { OpenSearchDatasource } from '../../datasource';
+
+const mockDatasource = {
+  getSupportedQueryTypes: () => [QueryType.Lucene, QueryType.PPL],
+} as OpenSearchDatasource;
+
+// Slate seems to cause an error without this, getSelection is not present in the current jsDom version.
+(window as any).getSelection = () => {};
+
 describe('QueryEditorForm', () => {
-  it('should render LuceneEditor when queryType is not set', () => {
-    const query: OpenSearchQuery = {
+  it('should render LuceneEditor given Lucene queryType', async () => {
+    let query: OpenSearchQuery = {
       refId: 'A',
-    };
-    const wrapper = shallow(<QueryEditorForm value={query} />);
-    expect(wrapper.find(LuceneEditor).length).toBe(1);
-    expect(wrapper.find(PPLEditor).length).toBe(0);
-  });
-
-  it('should render LuceneEditor given Lucene queryType', () => {
-    const luceneQuery: OpenSearchQuery = {
-      refId: 'A',
+      query: '',
       queryType: QueryType.Lucene,
+      metrics: [{ type: 'count', id: '2' }],
+      bucketAggs: [{ type: 'date_histogram', id: '1' }],
     };
-    const wrapper = shallow(<QueryEditorForm value={luceneQuery} />);
-    expect(wrapper.find(LuceneEditor).length).toBe(1);
-    expect(wrapper.find(PPLEditor).length).toBe(0);
+
+    render(<QueryEditor query={query} onChange={q => (query = q)} onRunQuery={() => {}} datasource={mockDatasource} />);
+
+    expect(screen.getByText('Lucene')).toBeInTheDocument();
+    expect(screen.queryByText('PPL')).not.toBeInTheDocument();
   });
 
-  it('should render PPLEditor given PPL queryType', () => {
-    const pplQuery: OpenSearchQuery = {
+  it('should render PPLEditor given PPL queryType', async () => {
+    let query: OpenSearchQuery = {
       refId: 'A',
+      query: '',
       queryType: QueryType.PPL,
+      metrics: [{ type: 'count', id: '2' }],
+      bucketAggs: [{ type: 'date_histogram', id: '1' }],
     };
-    const wrapper = shallow(<QueryEditorForm value={pplQuery} />);
-    expect(wrapper.find(LuceneEditor).length).toBe(0);
-    expect(wrapper.find(PPLEditor).length).toBe(1);
+
+    render(<QueryEditor query={query} onChange={q => (query = q)} onRunQuery={() => {}} datasource={mockDatasource} />);
+
+    expect(screen.getByText('PPL')).toBeInTheDocument();
+    expect(screen.queryByText('Lucene')).not.toBeInTheDocument();
   });
 });
