c21b2eaae517695ecad3467104a31ce7d36a23d5
Sven Grossmann <svennergr@gmail.com>
2022-10-17 15:28:10 +0200

    add missing implementation to add custom headers

--- pkg/opensearch/client/client.go
+++ pkg/opensearch/client/client.go
@@ -30,6 +30,8 @@ var (
 
 // TODO: use real settings for HTTP client
 var newDatasourceHttpClient = func(ds *backend.DataSourceInstanceSettings) (*http.Client, error) {
+	//dsHttpOpts, err := ds.HTTPClientOptions()
+
 	jsonDataStr := ds.JSONData
 	jsonData, err := simplejson.NewJson([]byte(jsonDataStr))
 	if err != nil {
@@ -285,6 +287,14 @@ func (c *baseClientImpl) executeRequest(method, uriPath, uriQuery string, body [
 	req.Header.Set("User-Agent", "Grafana")
 	req.Header.Set("Content-Type", "application/json")
 
+	dsHttpOpts, err := c.ds.HTTPClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range dsHttpOpts.Headers {
+		req.Header.Set(k, v)
+	}
+
 	secureJsonData := c.ds.DecryptedSecureJSONData
 
 	if c.ds.BasicAuthEnabled {
@@ -500,6 +510,13 @@ func (c *baseClientImpl) executePPLQueryRequest(method, uriPath string, body []b
 
 	req.Header.Set("User-Agent", "Grafana")
 	req.Header.Set("Content-Type", "application/json")
+	dsHttpOpts, err := c.ds.HTTPClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range dsHttpOpts.Headers {
+		req.Header.Set(k, v)
+	}
 
 	secureJsonData := c.ds.DecryptedSecureJSONData
 
