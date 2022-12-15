20918a9f9ffe1344778d36b0d09cbe8063512274
Sven Grossmann <svennergr@gmail.com>
2022-12-15 08:08:22 +0100

    add correct servicename to alerting

--- pkg/opensearch/client/client.go
+++ pkg/opensearch/client/client.go
@@ -82,8 +82,13 @@ func GetSigV4Config(ds *backend.DataSourceInstanceSettings) (*sigv4.Config, erro
 		return nil, err
 	}
 
+	serviceName := "es"
+	if jsonData.Get("serverless").MustBool() {
+		serviceName = "aoss"
+	}
+
 	sigV4Config := &sigv4.Config{
-		Service:       "es", // Always "es" for elasticsearch/opendistro/opensearch TODO: Check if this is correct
+		Service:       serviceName,
 		AccessKey:     decrypted["sigV4AccessKey"],
 		SecretKey:     decrypted["sigV4SecretKey"],
 		Region:        jsonData.Get("sigV4Region").MustString(),
