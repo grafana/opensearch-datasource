18a4f6997218565cadad3ee5947473b9ec1abe73
z0h3 <p0z1ck@yandex.ru>
2023-04-06 13:20:04 +0300

    Fix view of nested array field in table column

--- src/dependencies/flatten.ts
+++ src/dependencies/flatten.ts
@@ -12,7 +12,9 @@ export default function flatten(target: object, opts?: { delimiter?: any; maxDep
   function step(object: any, prev: string | null) {
     Object.keys(object).forEach(key => {
       const value = object[key];
-      const isarray = opts?.safe && Array.isArray(value);
+      const isSafe = opts?.safe;
+      const isarray = Array.isArray(value);
+      const isSafeArray = isSafe && isarray;
       const type = Object.prototype.toString.call(value);
       const isobject = type === '[object Object]';
 
@@ -22,12 +24,16 @@ export default function flatten(target: object, opts?: { delimiter?: any; maxDep
         maxDepth = currentDepth + 1;
       }
 
-      if (!isarray && isobject && Object.keys(value).length && currentDepth < maxDepth) {
+      if (!isSafeArray && isobject && Object.keys(value).length && currentDepth < maxDepth) {
         ++currentDepth;
         return step(value, newKey);
       }
 
-      output[newKey] = value;
+      if (isarray) {
+        output[newKey] = JSON.stringify(value);
+      } else {
+        output[newKey] = value;
+      }
     });
   }
 
