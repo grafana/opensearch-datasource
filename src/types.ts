79192fbd5fdc33758214e0ad0fb800516d8575c5
Sarah Zinger <sarah.zinger@grafana.com>
2023-04-20 13:54:57 -0400

    Add type for Traces Bucket Aggregations

--- src/types.ts
+++ src/types.ts
@@ -103,12 +103,48 @@ export enum LuceneQueryType {
   Metric = 'Metric',
 }
 
+export type AggsForTraces = {
+  traces: {
+    // each of those buckets in traces is sorted by a key of their traceId
+    // they contain any document, in this case all the spans of a trace
+    terms: {
+      field: 'traceId';
+      size: 100;
+      order: { _key: 'asc' };
+    };
+    // within each of those buckets we create further aggregations based on what's in that bucket
+    aggs: {
+      // one of those aggregations is a metric we call latency which is based on the durationInNanos
+      // this script was taken directly from the network tab in the traces dashboard
+      latency: {
+        max: {
+          script: {
+            source: "\n                if (doc.containsKey('traceGroupFields.durationInNanos') && !doc['traceGroupFields.durationInNanos'].empty) {\n                  return Math.round(doc['traceGroupFields.durationInNanos'].value / 10000) / 100.0\n                }\n                return 0\n                ";
+            lang: 'painless';
+          };
+        };
+      };
+      // one of those aggregations is the first traceGroup value it finds in the bucket
+      trace_group: {
+        terms: {
+          field: 'traceGroup';
+          size: 1;
+        };
+      };
+      // one of aggregations is the the number of items in the bucket that has a status code of 2
+      error_count: {
+        filter: { term: { 'traceGroupFields.statusCode': '2' } };
+      };
+      // one of those aggregations is the span with the max endTime
+      last_updated: { max: { field: 'traceGroupFields.endTime' } };
+    };
+  };
+};
+
 export type LuceneQueryObj = {
   size: number;
   query: DSLQuery;
-  // TODO we could type this further, default trace queries are very hard coded right now and do not change much.
-  // but maybe it makes more sense to revisit typing this once we support more than just traces
-  aggs?: unknown;
+  aggs?: AggsForTraces;
 };
 
 export type DSLQuery = {
