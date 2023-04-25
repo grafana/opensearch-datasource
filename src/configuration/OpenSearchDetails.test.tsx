d0b71bd025f89b96e1366f8de499de84c0c2fb3a
Isabella Siu <isabella.siu@grafana.com>
2023-04-25 16:01:24 -0400

    capitalize file name

--- src/configuration/OpenSearchDetails.test.tsx
+++ src/configuration/OpenSearchDetails.test.tsx
@@ -9,7 +9,7 @@ import { DataSourceSettings } from '@grafana/data';
 import { config } from '@grafana/runtime';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { setupMockedDataSource } from '__mocks__/openSearchDatasource';
+import { setupMockedDataSource } from '__mocks__/OpenSearchDatasource';
 const { Select, Switch } = LegacyForms;
 
 describe('OpenSearchDetails', () => {
