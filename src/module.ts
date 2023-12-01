aa618425eb7b9ecc721966de56e94ccbffbd5bc3
Ida Štambuk <ida.stambuk@grafana.com>
2023-12-01 11:43:55 +0100

    Upgrade dependencies and fix build errors

--- src/module.ts
+++ src/module.ts
@@ -2,11 +2,7 @@ import { DataSourcePlugin } from '@grafana/data';
 import { OpenSearchDatasource } from './datasource';
 import { ConfigEditor } from './configuration/ConfigEditor';
 import { QueryEditor } from './components/QueryEditor';
-import { OpenSearchQuery } from 'types';
-import { AwsAuthDataSourceJsonData } from '@grafana/aws-sdk';
-new DataSourcePlugin<XrayDataSource, XrayQuery, XrayJsonData, AwsAuthDataSourceSecureJsonData>(
-  XrayDataSource
-)
-export const plugin = new DataSourcePlugin<OpenSearchDatasource, OpenSearchQuery, {}, AwsAuthDataSourceJsonData>(OpenSearchDatasource)
+
+export const plugin = new DataSourcePlugin(OpenSearchDatasource)
   .setQueryEditor(QueryEditor)
   .setConfigEditor(ConfigEditor);
