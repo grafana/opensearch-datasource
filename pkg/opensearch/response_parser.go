db55915af122850c8e1db6f17a4ba9a0a82844c2
Isabella Siu <isabella.siu@grafana.com>
2024-05-01 15:44:41 -0400

    cleanup

--- pkg/opensearch/response_parser.go
+++ pkg/opensearch/response_parser.go
@@ -152,13 +152,9 @@ func processPrefetchResponse(res *client.SearchResponse, queryRes backend.DataRe
 
 func processTraceSpansResponse(res *client.SearchResponse, queryRes backend.DataResponse) backend.DataResponse {
 	propNames := make(map[string]bool)
-	var hits []map[string]interface{}
-	if res.Hits != nil {
-		hits = res.Hits.Hits
-	}
-	docs := make([]map[string]interface{}, len(hits))
+	docs := make([]map[string]interface{}, len(res.Hits.Hits))
 
-	for hitIdx, hit := range hits {
+	for hitIdx, hit := range res.Hits.Hits {
 		var withKeysToObj map[string]interface{}
 		if hit["_source"] != nil {
 			// some k:v pairs come from OpenSearch with field names in dot notation: 'span.attributes.http@status_code': 200,
