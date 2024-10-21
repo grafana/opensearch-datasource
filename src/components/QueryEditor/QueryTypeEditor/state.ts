144621fc0e52ac66e4843b2e78dd4c1ade3e6eaf
Kevin Yu <kevinwcyu@users.noreply.github.com>
2024-10-21 13:21:05 -0700

    update QueryEditor actions/reducers

--- src/components/QueryEditor/QueryTypeEditor/state.ts
+++ src/components/QueryEditor/QueryTypeEditor/state.ts
@@ -1,34 +1,19 @@
+import { createAction } from '@reduxjs/toolkit';
 import { Action } from '../../../hooks/useStatelessReducer';
 import { OpenSearchQuery, QueryType } from '../../../types';
-import { INIT, InitAction } from '../state';
+import { initQuery } from '../state';
 
 export const CHANGE_QUERY_TYPE = 'change_query_type';
 
-export interface ChangeQueryTypeAction extends Action<typeof CHANGE_QUERY_TYPE> {
-  payload: {
-    queryType: QueryType;
-  };
-}
+export const changeQueryType = createAction<OpenSearchQuery['queryType']>(CHANGE_QUERY_TYPE);
 
-export const changeQueryType = (queryType: QueryType): ChangeQueryTypeAction => ({
-  type: CHANGE_QUERY_TYPE,
-  payload: {
-    queryType,
-  },
-});
-
-export const queryTypeReducer = (
-  prevQueryType: OpenSearchQuery['queryType'],
-  action: ChangeQueryTypeAction | InitAction
-) => {
-  switch (action.type) {
-    case CHANGE_QUERY_TYPE:
-      return action.payload.queryType;
-
-    case INIT:
-      return QueryType.Lucene;
-
-    default:
-      return prevQueryType;
+export const queryTypeReducer = (prevQueryType: OpenSearchQuery['queryType'], action: Action) => {
+  if (changeQueryType.match(action)) {
+    return action.payload;
   }
+  if (initQuery.match(action)) {
+    return QueryType.Lucene;
+  }
+
+  return prevQueryType;
 };
