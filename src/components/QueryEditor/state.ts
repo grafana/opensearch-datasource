081f70e965a349430d2d6c9d6fe0ea8fad1e818f
Kevin Yu <kevinwcyu@users.noreply.github.com>
2024-10-21 13:21:40 -0700

    update shared actions/reducers

--- src/components/QueryEditor/state.ts
+++ src/components/QueryEditor/state.ts
@@ -1,3 +1,4 @@
+import { createAction } from '@reduxjs/toolkit';
 import { OpenSearchQuery } from 'types';
 import { Action } from '../../hooks/useStatelessReducer';
 
@@ -7,63 +8,34 @@ const CHANGE_ALIAS_PATTERN = 'change_alias_pattern';
 
 export interface InitAction extends Action<typeof INIT> {}
 
-interface ChangeQueryAction extends Action<typeof CHANGE_QUERY> {
-  payload: {
-    query: string;
-  };
-}
-
-interface ChangeAliasPatternAction extends Action<typeof CHANGE_ALIAS_PATTERN> {
-  payload: {
-    aliasPattern: string;
-  };
-}
-
 /**
  * When the `initQuery` Action is dispatched, the query gets populated with default values where values are not present.
  * This means it won't override any existing value in place, but just ensure the query is in a "runnable" state.
  */
-export const initQuery = (): InitAction => ({ type: INIT });
+export const initQuery = createAction(INIT);
 
-export const changeQuery = (query: string): ChangeQueryAction => ({
-  type: CHANGE_QUERY,
-  payload: {
-    query,
-  },
-});
+export const changeQuery = createAction<OpenSearchQuery['query']>(CHANGE_QUERY);
 
-export const changeAliasPattern = (aliasPattern: string): ChangeAliasPatternAction => ({
-  type: CHANGE_ALIAS_PATTERN,
-  payload: {
-    aliasPattern,
-  },
-});
+export const changeAliasPattern = createAction<OpenSearchQuery['alias']>(CHANGE_ALIAS_PATTERN);
 
-export const queryReducer = (prevQuery: OpenSearchQuery['query'], action: ChangeQueryAction | InitAction) => {
-  switch (action.type) {
-    case CHANGE_QUERY:
-      return action.payload.query;
-
-    case INIT:
-      return prevQuery || '';
-
-    default:
-      return prevQuery;
+export const queryReducer = (prevQuery: OpenSearchQuery['query'], action: Action) => {
+  if (changeQuery.match(action)) {
+    return action.payload;
   }
+  if (initQuery.match(action)) {
+    return prevQuery || '';
+  }
+
+  return prevQuery;
 };
 
-export const aliasPatternReducer = (
-  prevAliasPattern: OpenSearchQuery['alias'],
-  action: ChangeAliasPatternAction | InitAction
-) => {
-  switch (action.type) {
-    case CHANGE_ALIAS_PATTERN:
-      return action.payload.aliasPattern;
-
-    case INIT:
-      return prevAliasPattern || '';
-
-    default:
-      return prevAliasPattern;
+export const aliasPatternReducer = (prevAliasPattern: OpenSearchQuery['alias'], action: Action) => {
+  if (changeAliasPattern.match(action)) {
+    return action.payload;
   }
+  if (initQuery.match(action)) {
+    return prevAliasPattern || '';
+  }
+
+  return prevAliasPattern;
 };
