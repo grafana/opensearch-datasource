26df8111ecf3f48d6d05cb8446e0b0ca930b371c
Kevin Yu <kevinwcyu@users.noreply.github.com>
2025-05-20 11:10:51 -0700

    fix lint

--- pkg/opensearch/snapshot_tests/lucene_logs_test.go
+++ pkg/opensearch/snapshot_tests/lucene_logs_test.go
@@ -26,7 +26,11 @@ func Test_logs_request(t *testing.T) {
 				if err != nil {
 					return err
 				}
-				defer req.Body.Close()
+				defer func() {
+					if err := req.Body.Close(); err != nil {
+						t.Errorf("failed to close request body: %v", err)
+					}
+				}()
 				return nil
 			}},
 		},
