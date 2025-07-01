e2df826cc7f0e472daf84257b28860e2a5fb6a7d
idastambuk <ida.stambuk@grafana.com>
2025-07-01 12:29:22 +0200

    Add language support for syntax highlighting

---
+++ src/language/ppl/definition.ts
@@ -0,0 +1,12 @@
+import { LanguageDefinition } from '../monarch/register';
+
+import { OPENSEARCH_PPL_LANGUAGE_DEFINITION_ID } from './language';
+
+const openSearchPPLLanguageDefinition: LanguageDefinition = {
+  id: OPENSEARCH_PPL_LANGUAGE_DEFINITION_ID,
+  extensions: [],
+  aliases: [],
+  mimetypes: [],
+  loader: () => import('./language'),
+};
+export default openSearchPPLLanguageDefinition;
