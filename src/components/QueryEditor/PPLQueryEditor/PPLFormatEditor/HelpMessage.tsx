fdf238f0237e4e4b10e0cdeda4c77e49aef316ae
idastambuk <ida.stambuk@grafana.com>
2025-07-28 18:11:39 +0200

    Remove deprecated styles

--- src/components/QueryEditor/PPLQueryEditor/PPLFormatEditor/HelpMessage.tsx
+++ src/components/QueryEditor/PPLQueryEditor/PPLFormatEditor/HelpMessage.tsx
@@ -1,25 +1,43 @@
+import { css } from '@emotion/css';
+import { GrafanaTheme2 } from '@grafana/data';
+import { useStyles2 } from '@grafana/ui';
 import React from 'react';
 
-export const HelpMessage = () => (
-  <div data-testid="help-message" className="gf-form grafana-info-box">
-    <div>
-      <li> Table and Logs return any set of columns</li>
-      <li>
-        Time series returns
-        <ul>
-          <li>date, datetime, or timestamp datatype as time column</li>
-          <li>numeric datatype as values</li>
-        </ul>
-      </li>
-      <br />
-      Example PPL query for time series:
-      <br />
-      <code>
-        source=&lt;index&gt;
+export const HelpMessage = () => {
+  const styles = useStyles2(getStyles);
+
+  return (
+    <div data-testid="help-message" className={styles}>
+      <div>
+        <li> Table and Logs return any set of columns</li>
+        <li>
+          Time series returns
+          <ul>
+            <li>date, datetime, or timestamp datatype as time column</li>
+            <li>numeric datatype as values</li>
+          </ul>
+        </li>
         <br />
-        | eval dateValue=timestamp(timestamp)
-        <br />| stats count(response) by dateValue
-      </code>
+        Example PPL query for time series:
+        <br />
+        <code>
+          source=&lt;index&gt;
+          <br />
+          | eval dateValue=timestamp(timestamp)
+          <br />| stats count(response) by dateValue
+        </code>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+const getStyles = (theme: GrafanaTheme2) =>
+  css({
+    ul: {
+      paddingLeft: theme.spacing(1.5),
+    },
+    code: {
+      fontFamily: theme.typography.fontFamilyMonospace,
+      fontSize: theme.typography.bodySmall.fontSize,
+    },
+  });
