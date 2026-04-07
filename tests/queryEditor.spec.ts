7b9f2bbffdc650109e2336425e479d1324f7e93d
Kevin Yu <kevinwcyu@users.noreply.github.com>
2026-04-07 11:43:33 -0700

    fix e2e tests

--- tests/queryEditor.spec.ts
+++ tests/queryEditor.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('should render query editor', async ({ panelEditPage, selectors }) => {
+test('should render query editor', async ({ panelEditPage, selectors, grafanaVersion }) => {
+  const queryEditorRow = grafanaVersion.startsWith('13.0.0')
+    ? panelEditPage.getByGrafanaSelector('data-testid Query editor row').filter({
+        has: panelEditPage.getByGrafanaSelector('data-testid Query editor row title A'),
+      })
+    : panelEditPage.getQueryEditorRow('A');
+
   await panelEditPage.datasource.set('AWS OpenSearch');
-  await expect(
-    panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.QueryField.container)
-  ).toBeVisible();
+  await expect(queryEditorRow.getByTestId(selectors.components.QueryField.container)).toBeVisible();
 });
